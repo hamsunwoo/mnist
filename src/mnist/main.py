5c5aea328cecad1f49edd488c4b2833994fcfee9
sunwoo <summerham22@gmail.com>
2024-09-23 10:49:23 +0900

    fix host

--- src/mnist/main.py
+++ src/mnist/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.post("/files/")
 async def file_list():
-    conn = pymysql.connect(host="127.18.0.1",
+    conn = pymysql.connect(host="172.17.0.1",
                                  user='mnist',
                                  password='1234',
                                  database='mnistdb',
